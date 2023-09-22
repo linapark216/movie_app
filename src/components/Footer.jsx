635332a389b69f315294782b1e43dcef16d2e205
이코 <mc2ko@naver.com>
2023-09-22 14:24:11 +0900

    "간단한 설명 문구 추가"

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Footer = () => {
-	return <div></div>;
+	return <div>푸터부분입니다</div>;
 };
 
 export default Footer;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Header = () => {
-	return <div></div>;
+	return <div>헤더부분입니다</div>;
 };
 
 export default Header;
